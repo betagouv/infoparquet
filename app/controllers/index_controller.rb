24e4ca174af56820cfe2e1340fbcdf8f2013986b
Sylvain Mahé <sylv.mahe@gmail.com>
2021-12-01 13:39:59 +0100

    fix index redirection

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,7 @@
 class IndexController < ApplicationController
     def index
         if current_user
-            redirect_to controller: :d_s, action: :index
+            redirect_to controller: :ds, action: :index
         end
     end
 end
